624d7432efd79a8c782b07789ecbfe1b35408a47
bradgaddis <bradleygad@gmail.com>
2023-09-04 13:57:46 -0400

    update rules and add hangman display

--- Main.py
+++ Main.py
@@ -1,15 +1,20 @@
 import random
+import os
+from getpass import getpass
 
 chances: str
 word: str
 numguesses: str
 guesses = [] 
+num_wrong: int
 
 def Display_Rules() -> None:
     rules = """
     Player 1 gets to choose 5 words. 1 word will be choosen at random. Each word will be between 4 and 8 characters long
 
-    Player 2 must select letters in the word that is presented. It will be presented as a series of underscores. You get three extra guesses.
+    Player 2 must select letters in the word that is presented. It will be presented as a series of underscores.
+     
+    The number of wrong gueses you get is equal to the chosen word + 3. You get three extra guesses.
 
     For each letter that player two guesses wrong, an additional body part will be drawn in
 
@@ -24,10 +29,11 @@ def PickWords() -> list:
     """
     Player 1 chooses 5 words via the input. Returns a list
     """
+    os.system('cls' if os.name == 'nt' else 'clear')
     print("Player 1: Please choose 5 words between 4 and 8 characters long")
     wordList = []
     while len(wordList) < 5:
-        word = input()
+        word = getpass("Type a word: ")
         for letter in word:
             if not letter.isalpha() or letter.isnumeric():
                 print("Please only use alphabetical letters")
@@ -50,8 +56,10 @@ def SetUp(words: list) -> None:
     global chances 
     global numguesses
     global guesses 
+    global num_wrong    
 
     guesses = []
+    num_wrong = 0
     word = words[random.randrange(0,4)]
     chances = ''.join(["_" for l in range(len(word))])
     numguesses = 0
@@ -64,8 +72,12 @@ def TakeTurn() -> None:
     global chances
     global numguesses
     global guesses
+    global num_wrong
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-    print("The word you're looking for is ", " ".join([l + " " for l in chances]))
+    Display_Hangman()
+
+    print(" ".join([l + " " for l in chances]))
     if len(guesses) > 0:
         print("You've guessed: ", guesses)
     
@@ -79,6 +91,9 @@ def TakeTurn() -> None:
             index = check_guessed_letter(guess)
             if index > -1:
                 update_chances(guess, index)
+            else:
+                num_wrong += 1
+                print(num_wrong)
             break
     print("\n")
 
@@ -107,17 +122,15 @@ def GameLoop() -> None:
 
 def GameOver() -> bool:
     "Determines if the game should continue or not on each turn."
-    global numguesses
+    global num_wrong
+
     global word
-    if numguesses >= len(word) + 3 and "_" in chances:
-            print("You Lose.")
+    if num_wrong >= len(word) + 3 and "_" in chances:
             if Replay():
                 SetUp(PickWords())
             else:
                 return True
     elif not "_" in chances:
-        print("You Win!")
-        print(f"The word was: {chances}")
         if Replay():
             SetUp(PickWords())
         else: 
@@ -126,11 +139,28 @@ def GameOver() -> bool:
 
 def Replay() -> bool:
     "Checks to see if the player wants to continue after the game has ended"
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f"The word was: {word}")
+    Display_Hangman()
     replay = True if input("Try again? y/n: ") == "y" else False
     if not replay:
         print("See you next time!")
     return replay
 
+def Display_Hangman():
+    global num_wrong
+    gallos_top = "---\n    |\n    \n"
+
+
+    hangman = [" ", "-", "-", "-", "\n" , "|", " ", 
+            " ", " ", "|", "\n", " ", "-", "-", "-"
+            "\n", "/", "|", " ", " ", "|", "\\"
+            "\n"," ", "_", "_", "_",
+            "\n", "|", " ", " ", " ", "|", "\n"
+            ]
+
+    amount = hangman[0: int(num_wrong / len(hangman) * 100)]
+    print(gallos_top + "".join(amount))
 
 def Main() -> None:
     Display_Rules()
